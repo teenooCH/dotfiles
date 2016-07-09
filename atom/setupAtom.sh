6587f5ed45dab8dd48e16c92d4fd1e06f8f0ed19
tinuP <teenooCH@github.com>
2016-07-09 10:39:31 +0200

    Initial set of config files

---
+++ atom/setupAtom.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Setup Atom editor
+set -euf -o pipefail
+
+AtomHome="${HOME}/.atom/"
+AtomPackages="packages.txt"
+
+if [ -d "${AtomHome}" ]; then
+  cp ./config.cson ${AtomHome}
+  cp ./keymap.cson ${AtomHome}
+  cp ./styles.less ${AtomHome}
+fi
+
+while read -r pkg; do
+  apm install ${pkg}
+done <${AtomPackages}
