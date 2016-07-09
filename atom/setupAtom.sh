2b5187be300da713725fd3e33fa88c16cdc3d24a
tinuP <teenooCH@github.com>
2016-07-09 22:07:46 +0200

    Create atom config dir if necessary

--- atom/setupAtom.sh
+++ atom/setupAtom.sh
@@ -2,14 +2,17 @@
 # Setup Atom editor
 set -euf -o pipefail
 
-AtomHome="${1}/.atom/"
+AtomHome="/home/${1}/.atom/"
 AtomPackages="packages.txt"
 
-if [ -d "${AtomHome}" ]; then
-  cp ./config.cson ${AtomHome}
-  cp ./keymap.cson ${AtomHome}
-  cp ./styles.less ${AtomHome}
+if [ ! -d "${AtomHome}" ]; then
+  mkdir ${AtomHome}
+  chmod 0750 ${1} ${AtomHome}
+  chown ${1}:${1} ${AtomHome}
 fi
+cp ./config.cson ${AtomHome}
+cp ./keymap.cson ${AtomHome}
+cp ./styles.less ${AtomHome}
 
 while read -r pkg; do
   apm install ${pkg}
